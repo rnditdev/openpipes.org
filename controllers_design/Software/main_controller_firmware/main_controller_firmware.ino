1368f6d279f8ba7270ab7a8c732fb25cc9237ecb
Rodolfo Bonnin <15949642+rnditdev@users.noreply.github.com>
2023-06-06 11:51:13 -0300

    Updated controller firmware

--- controllers_design/Software/main_controller_firmware/main_controller_firmware.ino
+++ controllers_design/Software/main_controller_firmware/main_controller_firmware.ino
@@ -6,8 +6,18 @@ const int muxSIG2 = 14;
 const int muxSIG3 = 13;
 const int muxSIG4 = 12;
 
-int ports[]={15,14,16,10};
+uint32_t startTimer = millis();
+constexpr uint32_t timeDiff = 10000;
+int sendVolumeTimeFlag = 0;
 
+
+int ports[]={15,14,10,16};
+
+int volumecenters[]={454,511,460,460};
+int volumelow[]={200,200,200,200};
+int volumehigh[]={823,823,823,823};
+int enabledvolumesensors[]={0,1,0,0};
+int enabledbuttonsensors[]={1,1,1,1};
 const int buttonValues[4];
 
 const int muxS0 = 3;
@@ -20,9 +30,10 @@ int pedal=1;
 bool debug=true;
 int buttonState;
 int pedals[64];
+int volumepedals[4];
 int j, i;
 int ledPin=6;
-const uint16_t PixelCount = 32;
+const uint16_t PixelCount = 64;
 const uint8_t PixelPin = 6;
 int ledBrightness=50;
 int midiCh=1;
@@ -45,24 +56,56 @@ void noteOn(int cmd, int pitch, int velocity) {
   Serial1.write(velocity);
 }
 
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
+}
+
 void processKey(int notenumber, int keystate) {
    //notenumber=31-notenumber; //this code is for pedals
    //int note = 0x18 + notenumber;  //This code is for pedals
    int note=0x0 + notenumber;
    if (keystate == 0 && pedals[notenumber]==0) {
+       Serial.println("notenumber");
        Serial.println(notenumber);
     MidiUSB.sendMIDI({0x09, 0x90 | midiCh , note, 127});
     //strip.SetPixelColor(15-(notenumber/2), RgbColor(255, 0,0));
     //strip.Show();
     pedals[notenumber]=1;}
   else if (keystate == 1 && pedals[notenumber]==1) {
-    MidiUSB.sendMIDI({0x09, 0x90 | midiCh , note, 0});
+    MidiUSB.sendMIDI({0x09, 0x90 | midiCh , note, 1});
     pedals[notenumber]=0;
     //strip.SetPixelColor(15-(notenumber/2), RgbColor(0, 0,0));
     //strip.Show();
     }
     MidiUSB.flush();
 
+}
+
+void processVolume(int sensor, int volumeread) {
+   //notenumber=31-notenumber; //this code is for pedals
+   //int note = 0x18 + notenumber;  //This code is for pedals
+   int cc=0x0 + sensor;
+
+   int newvalue = (abs(0.0000040 *(volumehigh[sensor]-volumeread) *(volumehigh[sensor]-volumeread) * (volumehigh[sensor]-volumeread)));
+   if (newvalue < 0 ) {newvalue = 0;}
+   if (newvalue > 127 ) {newvalue = 127;}
+   
+
+   if ((millis() - startTimer) > timeDiff){
+     sendVolumeTimeFlag = 1;
+     startTimer=millis();
+     Serial.println(F("AAAAAAAAAAAAAAAAAAAAAAAAAA"));
+   }
+
+
+   if ( ((abs(volumeread -volumepedals[sensor])>5) && (enabledvolumesensors[sensor]==1)) ||   sendVolumeTimeFlag == 1 ) {
+       controlChange(0, cc, newvalue);
+       volumepedals[sensor]=volumeread;
+       MidiUSB.flush();
+       if (sendVolumeTimeFlag == 1 ) {sendVolumeTimeFlag=0;}
+       }
 }
 
 
@@ -92,37 +135,58 @@ void setup()
     pedals[i] = 0;
   }
 
+  for ( i =0; i<4; i++)
+  {
+    volumepedals[i] = 0;
+  }
   strip.Begin();
   strip.Show();
 
- for ( int a =0; a<32; a++)
+ for ( int a =0; a<PixelCount; a++)
   {
- 
-  strip.SetPixelColor(a, RgbColor(127, 127, 127));
+  strip.SetPixelColor(a, RgbColor(255, 208, 23));
   strip.Show();
-  delay(100);
+  delay(200);
   }
 }
 
 void checkKeys(){
    int note;
    for (int channel=0;channel < 4;channel++){
+     if (enabledbuttonsensors[channel]==0){break;}
+     Serial.println(channel);
      for (byte i = 0; i < 16; i++)
      {
         SetMuxChannel(i);
         int currentValue = digitalRead(ports[channel]);
+        Serial.print(i);
+        Serial.print(":");
         Serial.print(currentValue);
         Serial.print("\t");
-        processKey(channel*16+i , currentValue);
-        
+        processKey(channel*16+i , currentValue);  
      }
-
       Serial.print("\n");
 
    }
     Serial.println();
+    delay(10);
   }
 
+void checkVolumes(){
+   int note;
+   int initial_sensor_pin=18;
+   for (int sensor_pin=0;sensor_pin < 4;sensor_pin++){
+        int currentValue  = analogRead(initial_sensor_pin+sensor_pin);
+
+        processVolume(sensor_pin , currentValue);
+        Serial.print("sensor_pin:");
+        Serial.print(sensor_pin);
+        Serial.print("Value:");
+        Serial.print(currentValue);  
+        Serial.println();
+     }
+      Serial.println();
+  }
 
 void checkMidi() {
   midiEventPacket_t rx;
@@ -157,5 +221,6 @@ void checkMidi() {
 
 void loop() {
   checkKeys();
-  checkMidi(); 
+  checkMidi();
+  checkVolumes();
 }
